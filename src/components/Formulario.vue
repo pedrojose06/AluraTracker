16bea3e4b563afd1e5f6c0d31ace929a4aa17714
pedrojose06s <pedrojosetm16@gmail.com>
2022-10-04 16:48:56 -0300

    aula 1 atividade 05

---
+++ src/components/Formulario.vue
@@ -0,0 +1,36 @@
+<template>
+    <div class="box">
+        <div class="columns">
+            <div class="column is-8" role="form" aria-label="Formulário Teste">
+                <input type="text" class="input" placeholder="Qual tarefa deseja iniciar?" />
+            </div>
+
+            <div class="column">
+                <div class="is-flex is-align-items-center is-justify-content-space-between">
+                    <section>
+                        <strong>00:00:00</strong>
+                    </section>
+                    <button class="button">
+                        <span class="icon">
+                            <i class="fas fa-play"></i>
+                        </span>
+                        <span>play</span>
+                    </button>
+                    <button class="button">
+                        <span class="icon">
+                            <i class="fas fa-stop"></i>
+                        </span>
+                        <span>stop</span>
+                    </button>
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from "vue";
+export default defineComponent({
+    name: "FormularioVue",
+});
+</script>
