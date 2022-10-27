dcaa886dbe51121ae715609bd3f2efb7b5e89e27
pedrojose06s <pedrojosetm16@gmail.com>
2022-10-27 19:48:35 -0300

    C02A01AT03

--- src/App.vue
+++ src/App.vue
@@ -4,17 +4,7 @@
 			<BarraLateralVue @aoTemaAlterado="trocarTema" />
 		</div>
 		<div class="column is-three-quarter conteudo">
-			<FormularioVue @aoSalvarTarefa="salvarTarefa" />
-			<div class="lista">
-				<TarefaVue
-					v-for="(tarefa, index) in tarefas"
-					:key="index"
-					:tarefa="tarefa"
-				/>
-			<BoxVue v-if="listaVazia">
-				<div> Você não está muito produtivo hoje ! :'( </div>
-			</BoxVue>
-			</div>
+			<router-view></router-view>
 		</div>
 	</main>
 </template>
@@ -22,35 +12,19 @@
 <script lang="ts">
 	import { defineComponent } from 'vue';
 	import BarraLateralVue from './components/BarraLateral.vue';
-	import FormularioVue from './components/Formulario.vue';
-	import TarefaVue from './components/Tarefa.vue';
-	import ITarefa from './Interfaces/ITarefa';
-	import BoxVue from './components/Box.vue';
 
 
 	export default defineComponent({
 		name: 'App',
 		components: {
 			BarraLateralVue,
-			FormularioVue,
-			TarefaVue,
-			BoxVue,
 		},
 		data() {
 			return {
-				tarefas: [] as ITarefa[],
 				modoEscuroAtivo: false
 			}
 		},
-		computed:{
-			listaVazia() : boolean{
-				return this.tarefas.length === 0;
-			}
-		},
 		methods: {
-			salvarTarefa(tarefa: ITarefa) {
-				this.tarefas.push(tarefa);
-			},
 			trocarTema(modoEscuroAtivo: boolean){
 				this.modoEscuroAtivo = modoEscuroAtivo
 			}
